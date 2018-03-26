5e8a0e83c1e5fb3f2735798d6d445c9809a38e97
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-26 15:19:25 -0600

    added smooth scrolling to page

--- assets/scripts/collapsing.js
+++ assets/scripts/collapsing.js
@@ -7,3 +7,11 @@ function navbarCollapse() {
     }
 }
 
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 50
+    }, 500);
+});
+
